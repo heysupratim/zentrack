3433f48335a4bb7f69c13ea7a4457cdcd0264e88
Supratim Chakraborty <sup270792@gmail.com>
2019-11-12 15:55:31 -0800

    Initial Commit

---
+++ ZenTrack/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ZenTrack
+//
+//  Created by Supratim on 2019-11-12.
+//  Copyright © 2019 Supratim Chakraborty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
